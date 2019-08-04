d1bfd2264e42d1fcf142fb66fbd525af0846961b
Shamus Clifford <shamusclifford@bpsma.org>
2019-08-04 00:02:29 -0400

    Use Ganache GUI

--- truffle-config.js
+++ truffle-config.js
@@ -4,20 +4,21 @@ var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble s
 module.exports = {
   networks: {
     development: {
+      /*
       host: "127.0.0.1",
       port: "8545",
-      /*
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
       */
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
+      },
       network_id: '*',
-      gas: 9999999
+      gas: 999999999,
+      gasPrice: 1
     }
   },
   compilers: {
     solc: {
-      version: "^0.5.8"
+      version: "^0.5.10"
     }
   }
 };
